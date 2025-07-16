5f80b55f1223605e2c4d5c7432c8c8375b269aed
DESKTOP-2MTF3A5\lbotelho <l_botelho@hotmail.com>
2025-07-16 20:22:41 +0100

    add logs to function

--- OrderFunction/OrderFunction.cs
+++ OrderFunction/OrderFunction.cs
@@ -16,31 +16,40 @@ namespace OrderFunction
             [ServiceBusTrigger("orders", Connection = "ServiceBusConnection")] string myQueueItem,
             ILogger log)
         {
-            var connectionString = Environment.GetEnvironmentVariable("SqlConnection");
-
-            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-            optionsBuilder.UseSqlServer(connectionString);
-
-            using var dbContext = new AppDbContext(optionsBuilder.Options);
-
-            var order = JsonSerializer.Deserialize<Order>(myQueueItem);
-            if (order == null)
+            try
             {
-                log.LogWarning("Invalid message from service bus.");
-                return;
+                var connectionString = Environment.GetEnvironmentVariable("SqlConnection");
+
+                var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+                optionsBuilder.UseSqlServer(connectionString);
+
+                using var dbContext = new AppDbContext(optionsBuilder.Options);
+
+                var order = JsonSerializer.Deserialize<Order>(myQueueItem);
+                if (order == null)
+                {
+                    log.LogWarning("Invalid message from service bus.");
+                    return;
+                }
+
+                var existing = await dbContext.Orders.FindAsync(order.Id);
+                if (existing != null)
+                {
+                    existing.Status = "Processed";
+                    await dbContext.SaveChangesAsync();
+                    log.LogInformation($"Order {existing.Id} changed to 'Processed'.");
+                }
+                else
+                {
+                    log.LogWarning($"Order with ID {order.Id} not found.");
+                }
             }
-
-            var existing = await dbContext.Orders.FindAsync(order.Id);
-            if (existing != null)
+            catch (Exception ex)
             {
-                existing.Status = "Processed";
-                await dbContext.SaveChangesAsync();
-                log.LogInformation($"Order {existing.Id} changed to 'Processed'.");
-            }
-            else
-            {
-                log.LogWarning($"Order with ID {order.Id} not found.");
+                log.LogError(ex, "Failed to process message: " + myQueueItem);
+                throw;
             }
         }
     }
+
 }
